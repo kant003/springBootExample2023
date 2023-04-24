0df3d79a39fab9ec6ccbf3a6850ed337023faf49
kant003 <gonzalezm.angel@gmail.com>
2023-04-24 12:16:47 +0200

    ejercicio

--- src/main/java/com/cebem/rickandmorty/controllers/WebController.java
+++ src/main/java/com/cebem/rickandmorty/controllers/WebController.java
@@ -22,4 +22,9 @@ public class WebController {
     modelo.addAttribute("characters", charactersModel.results);
     return "rickandmortyall";
   }
+
+  // Queremos mostrar en una página web el total de personajes que tenemos
+  // Aparecerá la frase TOTAL DE PERSONAJE: 867
+  // Centrado en pantalla y de color verde.
+  // Usa un motor de plantallas (thymeleaf)
 }
