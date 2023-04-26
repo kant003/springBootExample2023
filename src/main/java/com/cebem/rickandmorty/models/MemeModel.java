b97e001883b2b6b316cf8b508c305f58fead298b
kant003 <gonzalezm.angel@gmail.com>
2023-04-26 12:53:01 +0200

    jpa configuration

---
+++ src/main/java/com/cebem/rickandmorty/models/MemeModel.java
@@ -0,0 +1,12 @@
+package com.cebem.rickandmorty.models;
+
+import java.util.Date;
+
+public class MemeModel {
+    long id;
+    String category;
+    String url;
+    String description;
+    String author;
+    Date createDate;
+}
