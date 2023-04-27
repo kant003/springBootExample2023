4ed569dbbd4571625f82adbe46279ed39ce8bfdc
kant003 <gonzalezm.angel@gmail.com>
2023-04-27 12:19:48 +0200

    jpa configuration

--- src/main/java/com/cebem/rickandmorty/models/MemeModel.java
+++ src/main/java/com/cebem/rickandmorty/models/MemeModel.java
@@ -2,16 +2,29 @@ package com.cebem.rickandmorty.models;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name="meme")
 public class MemeModel {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(unique = true, nullable=false)
     long id;
+
     String category;
     String url;
     String description;
     String author;
-    Date createDate;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    Date createdAt;
 }
