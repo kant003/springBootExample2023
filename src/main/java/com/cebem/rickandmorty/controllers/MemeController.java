c5b34edb934908b5c75b3af01a5289c48c140a4d
kant003 <gonzalezm.angel@gmail.com>
2023-05-05 12:14:18 +0200

    add new meme

--- src/main/java/com/cebem/rickandmorty/controllers/MemeController.java
+++ src/main/java/com/cebem/rickandmorty/controllers/MemeController.java
@@ -1,12 +1,14 @@
 package com.cebem.rickandmorty.controllers;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cebem.rickandmorty.models.MemeModel;
 import com.cebem.rickandmorty.services.MemeService;
@@ -24,9 +26,20 @@ public class MemeController {
     } 
 
     @PostMapping("/memes")
-    public String memesAdd(){
-        // cosasssss
-        return pendiente!!!!
+    public String memesAdd(@RequestParam Map<String, String> body){
+        String description = body.get("description");
+        String url = body.get("url");
+        String category = body.get("category");
+        String author = body.get("author");
+
+        MemeModel nuevoMeme = new MemeModel();
+        nuevoMeme.setDescription(description);
+        nuevoMeme.setUrl(url);
+        nuevoMeme.setCategory(category);
+        nuevoMeme.setAuthor(author);
+
+        memeService.createMeme(nuevoMeme);
+        return "memeOK";
     }
 
     @RequestMapping("/memes/addForm")
