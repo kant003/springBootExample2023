a2e7fdeb4ab5fccbe2cc9ed38a34c9d0b3f408d1
kant003 <gonzalezm.angel@gmail.com>
2023-05-30 10:58:53 +0200

    dockerfile

--- src/main/java/com/cebem/rickandmorty/controllers/VideoGameController.java
+++ src/main/java/com/cebem/rickandmorty/controllers/VideoGameController.java
@@ -1,5 +1,6 @@
 package com.cebem.rickandmorty.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
